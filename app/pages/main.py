cb34a594d9a38f3e2c274f8d20623ed2cdac2c0a
Davide Andreoli <davide.andreoli@icloud.com>
2025-11-12 13:36:43 +0100

    feat: add GitHub workflows for pull request checks and release process, and implement structured page navigation in main.py

--- app/pages/main.py
+++ app/pages/main.py
@@ -6,11 +6,14 @@ logging.basicConfig(level=logging.DEBUG)
 
 load_dotenv()
 
-pages = st.navigation(
-    {
-        "Home": ["home/home.py"],
-        "Resume": ["resume/resume_data.py", "resume/templates.py"],
-        "Chat": ["chat/chat.py"],
-    }
-)
-pages.run()
+pages = {
+    "Home": [st.Page("home/home.py", title="Home")],
+    "Resume": [
+        st.Page("resume/resume_data.py", title="Edit Resume"),
+        st.Page("resume/templates.py", title="Templates"),
+    ],
+    "Chat": [st.Page("chat/chat.py", title="Chat")],
+}
+
+pg = st.navigation(pages, expanded=True)
+pg.run()
